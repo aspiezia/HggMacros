3f27b32ba9ea3bb1179876d57b26b8e3434d03a3
Francesco <francesco.micheli@cern.ch>
2013-10-04 10:42:50 +0200

    removing unused branches. update of doDatacard

--- doDatacard.sh
+++ doDatacard.sh
@@ -7,10 +7,26 @@ root -b <<EOF
 CreateTree(true,"CS","",    "mass",80,100,180)
 CreateTree(true,"SS","cat6","mass",80,100,180)
 CreateTree(true,"SS","cat7","mass",80,100,180) 
+
+CreateTree(true,"SS","ttHhad","mass",80,100,180) 
+CreateTree(true,"SS","VHhadbtag","mass",80,100,180) 
+CreateTree(true,"SS","VHhad0tag","mass",80,100,180) 
+CreateTree(true,"CS","ttHhad","mass",80,100,180) 
+CreateTree(true,"CS","VHhadbtag","mass",80,100,180) 
+CreateTree(true,"CS","VHhad0tag","mass",80,100,180) 
+
 CreateTree(true,"CS","",    "diphobdt_output",200,-1,1)
 CreateTree(true,"SS","cat6","diphobdt_output",200,-1,1)
 CreateTree(true,"SS","cat7","diphobdt_output",200,-1,1)
 CreateTree(true,"SS","both","diphobdt_output",200,-1,1)
+
+CreateTree(true,"SS","ttHhad","diphobdt_output",200,-1,1) 
+CreateTree(true,"SS","VHhadbtag","diphobdt_output",200,-1,1) 
+CreateTree(true,"SS","VHhad0tag","diphobdt_output",200,-1,1) 
+CreateTree(true,"CS","ttHhad","diphobdt_output",200,-1,1) 
+CreateTree(true,"CS","VHhadbtag","diphobdt_output",200,-1,1) 
+CreateTree(true,"CS","VHhad0tag","diphobdt_output",200,-1,1) 
+
 
 .x MakeFit.cpp
 MakeFit(true,"cat6")
